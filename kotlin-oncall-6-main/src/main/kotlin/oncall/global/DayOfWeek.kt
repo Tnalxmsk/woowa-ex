803dfb3942d10bb3674555345c7984ef733f9f4d
Tnalxmsk <alstnwkd990@naver.com>
2024-12-09 23:17:09 +0900

    feat: DayOfWeek 객체 추가
    요일 관리 enum class 구현

---
+++ kotlin-oncall-6-main/src/main/kotlin/oncall/global/DayOfWeek.kt
@@ -0,0 +1,17 @@
+package oncall.global
+
+enum class DayOfWeek(val value: String) {
+    MONDAY("월"),
+    TUESDAY("화"),
+    WEDNESDAY("수"),
+    THURSDAY("목"),
+    FRIDAY("금"),
+    SATURDAY("토"),
+    SUNDAY("일");
+
+    companion object {
+        fun from(dayOfWeek: String): DayOfWeek {
+            return entries.find { it.value == dayOfWeek }!!
+        }
+    }
+}
