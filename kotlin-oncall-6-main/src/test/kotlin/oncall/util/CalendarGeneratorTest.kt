e547788e365180ebce75a686c34efe5cd9e2de2e
Tnalxmsk <alstnwkd990@naver.com>
2024-12-10 16:44:10 +0900

    test: 공휴일 적용 하도록 개선된 코드 테스트

--- kotlin-oncall-6-main/src/test/kotlin/oncall/util/CalendarGeneratorTest.kt
+++ kotlin-oncall-6-main/src/test/kotlin/oncall/util/CalendarGeneratorTest.kt
@@ -7,16 +7,16 @@ class CalendarGeneratorTest {
 
     @Test
     fun `달력을 생성한다`() {
-        val month = 4
-        val startDayOfWeek = "화"
+        val month = 5
+        val startDayOfWeek = "월"
         val days = CalendarGenerator.generateCalendar(month, startDayOfWeek)
 
         val expected = listOf(
-            "화", "수", "목", "금", "토", "일", "월",
-            "화", "수", "목", "금", "토", "일", "월",
-            "화", "수", "목", "금", "토", "일", "월",
-            "화", "수", "목", "금", "토", "일", "월",
-            "화", "수"
+            "월", "화", "수", "목", "금(휴일)", "토", "일",
+            "월", "화", "수", "목", "금", "토", "일",
+            "월", "화", "수", "목", "금", "토", "일",
+            "월", "화", "수", "목", "금", "토", "일",
+            "월", "화", "수"
         )
 
         assertThat(days).isEqualTo(expected)
