b10ded25c5de9b2a6b6c8604d500e0a6b8c4883d
Tnalxmsk <alstnwkd990@naver.com>
2024-12-10 16:43:58 +0900

    refactor: 공휴일 적용 하도록 로직 개선

--- kotlin-oncall-6-main/src/main/kotlin/oncall/util/CalendarGenerator.kt
+++ kotlin-oncall-6-main/src/main/kotlin/oncall/util/CalendarGenerator.kt
@@ -2,6 +2,7 @@ package oncall.util
 
 import oncall.global.Date
 import oncall.global.DayOfWeek
+import oncall.global.Holiday
 
 object CalendarGenerator {
     fun generateCalendar(month: Int, startDayOfWeek: String): List<String> {
@@ -10,6 +11,10 @@ object CalendarGenerator {
         val days = mutableListOf<String>()
         for (day in 1..date.days) {
             val nowDayOfWeek = DayOfWeek.entries[(dayOfWeek.ordinal + (day - 1)) % 7]
+            if (Holiday.isHoliday(month, day) && DayOfWeek.isWeekday(nowDayOfWeek.value)) {
+                days.add("${nowDayOfWeek.value}(휴일)")
+                continue
+            }
             days.add(nowDayOfWeek.value)
         }
 
