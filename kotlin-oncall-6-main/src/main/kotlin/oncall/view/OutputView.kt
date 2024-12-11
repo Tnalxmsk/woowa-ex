7c75a781aa86d7fa4f6dbfdef394a2496c3aec82
Tnalxmsk <alstnwkd990@naver.com>
2024-12-11 21:50:45 +0900

    refactor: 불필요 import 삭제

--- kotlin-oncall-6-main/src/main/kotlin/oncall/view/OutputView.kt
+++ kotlin-oncall-6-main/src/main/kotlin/oncall/view/OutputView.kt
@@ -1,7 +1,5 @@
 package oncall.view
 
-import oncall.global.DayOfWeek
-
 class OutputView {
     fun printDailyWorker(month: Int, day: Int, dayOfWeek: String, name: String) {
         println("${month}월 ${day}일 $dayOfWeek $name")
