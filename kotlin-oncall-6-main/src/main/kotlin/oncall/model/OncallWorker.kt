467589feeb1838174090125d1f6876b02f9de3d0
Tnalxmsk <alstnwkd990@naver.com>
2024-12-11 21:32:41 +0900

    refactor: String -> Worker 변경

--- kotlin-oncall-6-main/src/main/kotlin/oncall/model/OncallWorker.kt
+++ kotlin-oncall-6-main/src/main/kotlin/oncall/model/OncallWorker.kt
@@ -3,6 +3,6 @@ package oncall.model
 import java.util.Queue
 
 data class OncallWorker(
-    val weekdayWorkers: Queue<String>,
-    val weekendWorkers: Queue<String>
+    val weekdayWorkers: Queue<Worker>,
+    val weekendWorkers: Queue<Worker>
 )
