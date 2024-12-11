77f6633490a166b2608c2f06dc11a1e36f231288
Tnalxmsk <alstnwkd990@naver.com>
2024-12-11 19:19:20 +0900

    feat: DayOfWeek 기능 추가
    다음 요일을 구하는 기능 구현

--- kotlin-oncall-6-main/src/main/kotlin/oncall/global/DayOfWeek.kt
+++ kotlin-oncall-6-main/src/main/kotlin/oncall/global/DayOfWeek.kt
@@ -11,11 +11,17 @@ enum class DayOfWeek(val value: String) {
 
     companion object {
         fun from(dayOfWeek: String): DayOfWeek {
-            return entries.find { it.value == dayOfWeek }!!
+            val replaced = dayOfWeek.replace("(휴일)", "")
+            return entries.find { it.value == replaced }!!
         }
 
         fun isWeekday(dayOfWeek: String): Boolean {
             return dayOfWeek != SATURDAY.value && dayOfWeek != SUNDAY.value
+        }
+
+        fun nextDayOfWeek(dayOfWeek: String): DayOfWeek {
+            val nextDayOfWeek = entries[(from(dayOfWeek).ordinal + 1) % 7]
+            return nextDayOfWeek
         }
     }
 }
