13e1c5206a5027813896c82c83d65a785b168164
Tnalxmsk <alstnwkd990@naver.com>
2024-12-11 19:22:26 +0900

    feat: InputView Class 기능 구현
    평일 및 휴일 비상 근무자 입력 기능 구현

--- kotlin-oncall-6-main/src/main/kotlin/oncall/view/InputView.kt
+++ kotlin-oncall-6-main/src/main/kotlin/oncall/view/InputView.kt
@@ -8,4 +8,16 @@ class InputView {
         val input = Console.readLine()
         return input
     }
+
+    fun readWeekdayWorkers(): List<String> {
+        print("평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ")
+        val input = Console.readLine()
+        return input.split(",")
+    }
+
+    fun readWeekendWorkers(): List<String> {
+        print("주말 비상 근무 순번대로 사원 닉네임을 입력하세요> ")
+        val input = Console.readLine()
+        return input.split(",")
+    }
 }
